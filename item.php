04dd1b41dc7b75041c51f8e70677e4d3ece9737a
Marguerite MASSON <marguerite.masson@Digital-Grenoble.local>
2022-01-25 15:14:27 +0100

    Etape 2.1

--- item.php
+++ item.php
@@ -1,11 +1,10 @@
 <?php
-$nom_article = "Nom de l'article";
-$prix_article = "Prix de l'article";
-$photo_article = "Photo de l'article";
-echo "\nNom de l'article";
-echo "\n Prix de l'article";
-echo "\n Photo de l'article";
-
-echo "\n" .$nom_article;
-echo "\n" . $prix_article;
-echo $photo_article;
+$name_article = "Nom de l'article";
+$price_article = "Prix de l'article";
+$img_article = "papaya.jpg";
+echo $name_article;
+echo "<br>";
+echo $price_article;
+echo "<br>";
+echo "<img src =\"$img_article\">";
+?>
