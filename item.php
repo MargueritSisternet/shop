94f8d63b18f576aff1d1eecfff9c4363b863b2e3
Marguerite MASSON <marguerite.masson@Digital-Grenoble.local>
2022-01-25 14:28:17 +0100

    version 1

---
+++ item.php
@@ -0,0 +1,11 @@
+<?php
+$nom_article = "Nom de l'article";
+$prix_article = "Prix de l'article";
+$photo_article = "Photo de l'article";
+echo "\nNom de l'article";
+echo "\n Prix de l'article";
+echo "\n Photo de l'article";
+
+echo "\n" .$nom_article;
+echo "\n" . $prix_article;
+echo $photo_article;
